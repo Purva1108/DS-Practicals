b6b33723409e397da241b7c615ded987faf8f930
Purva Patel <purvapatel0101@gmail.com>
2026-09-17 16:14:51 +0530

    Implement postfix expression evaluation in C++

---
+++ Practical 4/infix_to_postfix.cpp
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+// Push operation
+void push(char item)
+{
+    stack[++top] = item;
+}
+
+// Pop operation
+char pop()
+{
+    if (top == -1)
+        return '\0';
+
+    return stack[top--];
+}
+
+// Return precedence of operators
+int precedence(char op)
+{
+    switch (op)
+    {
+        case '^':
+            return 3;
+
+        case '*':
+        case '/':
+            return 2;
+
+        case '+':
+        case '-':
+            return 1;
+
+        default:
+            return 0;
+    }
+}
+
+int main()
+{
+    char infix[MAX], postfix[MAX];
+    int i, j = 0;
+
+    printf("Enter infix expression: ");
+    scanf("%s", infix);
+
+    for (i = 0; infix[i] != '\0'; i++)
+    {
+        char ch = infix[i];
+
+        // Operand
+        if (isalnum(ch))
+        {
+            postfix[j++] = ch;
+        }
+
+        // Left parenthesis
+        else if (ch == '(')
+        {
+            push(ch);
+        }
+
+        // Right parenthesis
+        else if (ch == ')')
+        {
+            while (top != -1 && stack[top] != '(')
+            {
+                postfix[j++] = pop();
+            }
+
+            pop(); // Remove '('
+        }
+
+        // Operator
+        else
+        {
+            while (top != -1 &&
+                   precedence(stack[top]) >= precedence(ch))
+            {
+                postfix[j++] = pop();
+            }
+
+            push(ch);
+        }
+    }
+
+    // Pop remaining operators
+    while (top != -1)
+    {
+        postfix[j++] = pop();
+    }
+
+    postfix[j] = '\0';
+
+    printf("Postfix Expression: %s\n", postfix);
+
+    return 0;
+}
